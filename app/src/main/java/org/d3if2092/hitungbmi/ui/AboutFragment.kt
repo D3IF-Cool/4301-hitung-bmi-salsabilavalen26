403286203d60d2ec50aee4762a11931a41bb1a47
Nurlailiyah Salsabilah Valentina <salsabilavalen@student.telkomuniversity.ac.id>
2021-03-16 14:06:19 -0700

    Membuat navigasi menggunakan menu

---
+++ app/src/main/java/org/d3if2092/hitungbmi/ui/AboutFragment.kt
@@ -0,0 +1,6 @@
+package org.d3if2092.hitungbmi.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if2092.hitungbmi.R
+
+class AboutFragment : Fragment(R.layout.fragment_about)
--- app/src/main/java/org/d3if2092/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if2092/hitungbmi/ui/HitungFragment.kt
@@ -2,12 +2,11 @@ package org.d3if2092.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if2092.hitungbmi.R
 import org.d3if2092.hitungbmi.data.KategoriBmi
 import org.d3if2092.hitungbmi.databinding.FragmentHitungBinding
@@ -19,8 +18,23 @@ class HitungFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
         binding.button.setOnClickListener{ hitungBmi() }
-        binding.saranButton.setOnClickListener {view: View -> view.findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi))}
+        binding.saranButton.setOnClickListener {view: View ->
+            view.findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi))}
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun hitungBmi() {
